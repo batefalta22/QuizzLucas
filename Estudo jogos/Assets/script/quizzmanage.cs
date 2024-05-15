3be1cf0a0850cdd9b327783a08e826eb0dc98f35
batefalta22 <167206128+batefalta22@users.noreply.github.com>
2024-05-15 11:49:50 -0300

    primeiro commit

---
+++ Estudo jogos/Assets/script/quizzmanage.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quizzmanage : MonoBehaviour
+{
+    [Header("Variaveis de Painel")]
+    public GameObject painelInicio;
+    public GameObject painelJogo;
+
+    [Header("objetos do Jogo")]
+    public TMP_Text textoTitulo;
+    public Image imagemQuizz;
+    public TMP_Text textoPergunta;
+    public TMP_Text[] textoResposta;
+
+    [Header("Conteúdo as perguntas")]
+    public string[] titulos;
+    public Sprite[] imagens;
+    public string[] perguntas;
+    public string[] alternativa1;
+    public string[] alternativa2;
+    public string[] alternativa3;
+    public string[] alternativa4;
+    public int[] alternativaCorreta;
+    public int perguntaAtual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        painelInicio.SetActive(true);
+        painelJogo.SetActive(false);
+    }
+
+    //Método para iniciar o jogo
+    public void IniciarJogo()
+    {
+        painelInicio.SetActive(false);
+        painelJogo.SetActive(true);
+        ProximaPergunta(perguntaAtual);
+
+    }
+
+    //Método para fazer perguntas
+    public void ProximaPergunta(int numero)
+    {
+        textoTitulo.text = titulos[numero];
+        imagemQuizz.sprite = imagens[numero];
+        textoPergunta.text = perguntas[numero];
+        textoResposta[0].text = alternativa1[numero];
+        textoResposta[1].text = alternativa2[numero];
+        textoResposta[2].text = alternativa3[numero];
+        textoResposta[3].text = alternativa4[numero];
+    }
+    //Método para checar as perguntas
+    public void ChecarResposta(int numero)
+    {
+
+        StartCoroutine(ValidarResposta(numero));
+       
+    }
+
+    //Co rotina para mostrar se acertou ou errou
+    IEnumerator ValidarResposta(int numero)
+    {
+        if (numero == alternativaCorreta[perguntaAtual])
+        {
+            imagemQuizz.color = Color.green;
+        }
+        else
+        {
+            imagemQuizz.color = Color.red;
+        }
+        yield return new WaitForSeconds(2);
+
+        imagemQuizz.color = Color.white;
+        perguntaAtual++;
+        if (perguntaAtual >= titulos.Length)
+        {
+            painelInicio.SetActive(true);
+            painelJogo.SetActive(false);
+            perguntaAtual = 0;
+        }
+        else
+        {
+            ProximaPergunta(perguntaAtual);
+        }
+    }
+
+}
